ab40c177d1b895d18ead110334314c81bfd1ac60
gr2ws <gianasunan@gmail.com>
2023-03-04 23:06:17 +0800

    Fix some formatting

--- get_img.py
+++ get_img.py
@@ -1,11 +1,12 @@
 from os import path
 
+
 def get_image():
     """
-    Gets the file name of the image to be abstracted.
+    Prompts the user for the file name of the image to be abstracted.
 
     Returns:
-        image_name(str) - name of the image
+         the name of the image that the user input
     """
 
     image_name = input("Enter the name of the image. Make sure that it is in the 'img' folder."
@@ -15,14 +16,14 @@ def get_image():
 
 def image_exist(img_folder, img_name):
     """
-    Determines if the image exists inside the img directory .
+    Determines if the image exists inside the img directory.
 
-    Parameters:
-        img_folder(str) - the default image folder.
-        img_name(str) - name of the file.
+    Args:
+        img_folder (str): the default image folder.
+        img_name (str): name of the file.
 
     Returns:
-        (bool) - True if it does, False if not
+        (bool): True if exists, otherwise False
     """
 
     return path.exists(f"{img_folder}\\{img_name}")
