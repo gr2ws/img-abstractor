3817c80dd6f4d9434650dbab877720441fea8f72
gr2ws <gianasunan@gmail.com>
2023-03-06 11:48:20 +0800

    add painter and canvas, to fix rgb coordinates

--- image_work.py
+++ image_work.py
@@ -7,7 +7,8 @@ def get_filename():
     Prompts the user for the file name of the image to be abstracted.
 
     Returns:
-         (str): the name of the image that the user input
+         (str): the name of the image that the user input.
+
     """
 
     image_name = input("Enter the name of the image. Make sure that it is in the 'img' folder."
@@ -35,6 +36,7 @@ def get_pixels(img_loc, root_num_tiles):
     Gets a tiles x tiles size 2d array representing the rgb values of a specific (x, y) point in an image.
     Divides the image evenly into the user desired number of tiles, sampling a point using said number as an increment.
     The total number of tiles is the root_num_tiles ^ 2.
+
     Args:
         img_loc (str): the relative path of the image.
         root_num_tiles (int): the number of tiles to be painted by row, for a number of times.
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from image_work import *
+from turtle import Turtle, Screen
 
 # PROCURE IMAGE
 img_default = "img"
 img_name = get_filename()
-root_num_tiles = 512  # since image is 1:1, one size for width and height of the painting
+root_num_tiles = 16  # since image is 1:1, one size for width and height of the painting
 
 while not image_exist(img_default, img_name):
     print(f"Image not found! Please make sure it is in the '{img_default}' folder and that the file name is correct.")
@@ -12,4 +13,43 @@ while not image_exist(img_default, img_name):
 img_loc = f"{img_default}\\{img_name}"
 
 # GET RGB VALUES
-get_pixels(img_loc, root_num_tiles)
+image_rgb = get_pixels(img_loc, root_num_tiles)
+
+# READY CANVAS
+max_canvas_height, max_canvas_width = 512, 512
+canvas = Screen()
+canvas.screensize(max_canvas_height, max_canvas_width)
+row_step, column_step = int(max_canvas_height / root_num_tiles), int(max_canvas_width / root_num_tiles)
+canvas.colormode(255)
+
+# READY TURTLE
+painter = Turtle()
+painter.resizemode("auto")
+painter.shapesize(.5, .5, .5)
+painter.shape("circle")
+painter.speed(0)
+
+# PAINT
+start_height, start_width = (int(max_canvas_height / 2)), (int(max_canvas_height / 2))
+
+row = 0
+paint_row = 0
+
+painter.penup()
+painter.goto(start_height, start_width)
+painter.hideturtle()
+
+while paint_row < root_num_tiles:
+    column = 0
+    paint_column = 0
+    while paint_column < root_num_tiles:
+        print(paint_row, paint_column)
+        painter.color(image_rgb[paint_row][paint_column])
+        painter.goto(start_width - column, start_height - row,)
+        painter.stamp()
+        paint_column += 1
+        column += column_step
+    paint_row += 1
+    row += row_step
+
+canvas.exitonclick()
